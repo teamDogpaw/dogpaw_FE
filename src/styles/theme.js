6f6f7e29705583c2f54180f82b7f3c73c4e6c1e7
sparrowscout <sparrowscout.0000@gmail.com>
2022-07-07 02:11:50 +0900

    [Update] Write.js

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,15 +1,21 @@
 export const darkTheme = {
     textColor: "#fff",
+    textColor_sub: "#e2e2e2",
     backgroundColor: "#050505",
     divBackGroundColor: "#131313",
     keyColor:"#FFB673",
-    inputBoxBackground:"transparent"
+    inputBoxBackground:"#131313",
+    textareaBackground:"#131313",
+    border:"1px solid #777777"
 }
 
 export const lightTheme = {
     textColor: "#292929",
+    textColor_sub: "#292929",
     backgroundColor: "#FFF8E5",
     divBackGroundColor: "#fff",
     keyColor:"#FFB673",
-    inputBoxBackground:"white"
+    inputBoxBackground:"white",
+    textareaBackground:"#fbfbfb",
+    border:"1px solid #e2e2e2"
 }
