e268861631440d49e7332b25c7e728946b4d921e
jeehye03 <ks6247@naver.com>
2022-07-06 14:33:09 +0900

    [Update] mainPage

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,14 +1,19 @@
 import styled from "styled-components";
+import { ReactComponent as BookmarkIcon } from "../styles/icon/u_bookmark.svg";
+import { ReactComponent as BookmarkFill } from "../styles/icon/Vector 33.svg";
 
 import axios from "axios";
 import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
 import Comments from "../components/Comments";
+import { useState } from "react";
 
 const Detail = () => {
-  const params = useParams();
+  const [mark, setMark] = useState(false);
 
+  const params = useParams();
   const id = params.id;
+
   const getPostList = () => {
     return axios.get(`http://localhost:5001/allpost/${id}`);
   };
@@ -24,81 +29,161 @@ const Detail = () => {
     return null;
   }
 
+  const bookMark = () => {
+    //  북마크 체크
+    if (mark === false) {
+      setMark(true);
+    } else {
+      setMark(false);
+    }
+  };
   const content = detailQuery.data.data;
   //console.log(content);
   return (
-    <div>
-      <div>
-        <p>{content.title}</p>
+    <Wrap>
+      <ArticleTop>
+        <h1>{content.title}</h1>
         <User>
           <Img src={content.profileImg} alt="profile" />
           <p>{content.nickname}</p>
         </User>
+        <Mark>
+          {mark ? (
+            <BookmarkFill onClick={bookMark} />
+          ) : (
+            <BookmarkIcon onClick={bookMark} />
+          )}
+        </Mark>
+
+        <hr style={{ width: "100%", color: "#E2E2E2" }} />
         <ContentWrap>
           <Title>
             <p>진행방식</p>
             <p>구인스택</p>
             <p>예상 진행 기간</p>
-            <p>시장 예정일</p>
+            <p>시작 예정일</p>
             <p>모집 인원</p>
           </Title>
-          <div>
-          <p> {content.online}</p>
-          <Stack>
-            {content.stacks.map((lang, idx) => (
-              <p key={idx}> #{lang}</p>
-            ))}
-          </Stack>
+          <Content>
+            <p> {content.online}</p>
+            <Stack>
+              {content.stacks.map((lang, idx) => (
+                <p key={idx}> #{lang}</p>
+              ))}
+            </Stack>
 
-          <p>{content.period}</p>
-          <p> {content.startAt}</p>
-          <p> {content.capacity} 명</p>
-          </div>
-          <button>프로젝트 지원하기</button>
+            <p>{content.period}</p>
+            <p> {content.startAt}</p>
+            <p> {content.capacity} 명</p>
+          </Content>
+          <Button>프로젝트 지원하기</Button>
         </ContentWrap>
-
-        
-      </div>
+      </ArticleTop>
 
       <Article>
-        <p>프로젝트 소개</p>
+        <h1>프로젝트 소개</h1>
         <div>{content.content}</div>
       </Article>
 
-      <Comments/>
-    </div>
+      <Comments />
+    </Wrap>
   );
 };
+const Wrap = styled.div`
+  max-width: 996px;
+  //height:100vh;
+  margin: auto;
+
+  box-sizing: border-box;
+  h1 {
+    font-size: 25px;
+  }
+
+  p {
+    font-size: 16px;
+  }
+
+  @media screen and (max-width: 996px) {
+    margin: 0px 40px;
+  }
+`;
+const ArticleTop = styled.article`
+  background-color: white;
+  margin: auto;
+  border-radius: 16px;
+  padding: 32px;
+  position: relative;
+`;
 
 const User = styled.div`
   display: flex;
   align-items: center;
+  margin: 20px 0;
 `;
 
 const Img = styled.img`
   width: 40px;
   height: 40px;
   border-radius: 50%;
+  margin-right: 10px;
+`;
+
+const Mark = styled.div`
+  position: absolute;
+  top: 25px;
+  right: 30px;
 `;
 
 const ContentWrap = styled.div`
-display:flex;
-
-button{
-   height:30px;
-}
+  display: flex;
+  padding-top: 10px;
+  line-height: 48px;
+  position: relative;
+  // background-color:olive;
 `;
 
-const Title = styled.div`
+const Title = styled.div``;
 
+const Content = styled.div`
+  margin-left: 15px;
 `;
 
 const Stack = styled.div`
   display: flex;
+  align-items: center;
+  
+
+  p {
+    padding: 0px 15px;
+    background: #fff6c6;
+    border-radius: 24px;
+    margin-right: 10px;
+    text-align: center;
+    
+  }
 `;
 
-const Article = styled.article`
-margin-top:30px;
-
+const Article = styled(ArticleTop)`
+  margin-top: 30px;
+  div {
+    padding-top: 20px;
+  }
 `;
+
+const Button = styled.button`
+  height: 53px;
+  width: 200px;
+  background-color: #ffb673;
+  border: none;
+  border-radius: 8px;
+  color: #fff;
+  padding: 16px 24px;
+  font-size: 20px;
+  font-weight: 700;
+  position: absolute;
+  right: 10px;
+  bottom: 0px;
+  cursor: pointer;
+`;
+
 export default Detail;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -4,22 +4,25 @@ import { useQuery } from "react-query";
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-import styled from "styled-components";
-import u_comment from "../styles/icon/u_comment-alt-lines.svg";
+import styled, { css } from "styled-components";
+import {ReactComponent as CommentIcon} from "../styles/icon/u_comment-alt-lines.svg";
 import { ReactComponent as BookmarkIcon } from "../styles/icon/u_bookmark.svg";
-import {ReactComponent as BookmarkFill} from "../styles/icon/Vector 33.svg";
-
+import { ReactComponent as BookmarkFill } from "../styles/icon/Vector 33.svg";
 
 const Main = () => {
   const navigate = useNavigate();
-  const [mark, setMark] = useState(false);
+  const [mark, setMark] = useState(false); 
+  const [toggle, setToggle] = useState(true);
 
   const getPostList = () => {
     return axios.get("http://localhost:5001/allpost");
   };
 
-  const { isLoading, isError, data, error } = useQuery( "postList",getPostList, {
-   refetchOnWindowFocus: false,
+  const { isLoading, isError, data, error } = useQuery(
+    "postList",
+    getPostList,
+    {
+      refetchOnWindowFocus: false,
       onSuccess: (data) => {
         console.log(data);
       },
@@ -28,13 +31,6 @@ const Main = () => {
       },
     }
   );
-  //   const mainQuery = useQuery("postList", getPostList, {
-  //      refetchOnWindowFocus: false, // 사용자가 다른 곳에 갔다가 돌아올시 함수 재실행 여부
-  //     onSuccess: (data) => {
-  //       console.log("성공했어!", data);
-  //     },
-  //   });
-
   if (isLoading) {
     return <span>Loding...</span>;
   }
@@ -43,7 +39,7 @@ const Main = () => {
     return <span>Error:{error.message}</span>;
   }
 
- console.log(data.data)
+  //console.log(data.data);
 
   const bookMark = () => {
     if (mark === false) {
@@ -52,85 +48,188 @@ const Main = () => {
       setMark(false);
     }
   };
-  console.log(mark);
+  //console.log(mark);
+
+  const clickedToggle = () => {
+    setToggle((prev) => !prev);
+  };
 
   return (
-    <Wrap>
-    <Link to="/write">글쓰러 가기</Link>
-      {data.data.map((list) => {
-        return (
-          <div
-            key={list.id}
-            // onClick={() => {
-            //   navigate("/detail/" + list.id);
-            // }}
-          >
-            {list.title}
-            <TitleWrap>
-              <Img src={list.profileImg} alt="profileImg" />
-              <p>{list.nickname}</p>
-              {mark ? (
+    <>
+      <Link to="/write">글쓰러 가기</Link>
+      <ToggleWrap>
+        <h3>모집중만 보기</h3>
+        <ToggleBtn onClick={clickedToggle} toggle={toggle}>
+          <Circle toggle={toggle} />
+        </ToggleBtn>
+        {/* <h3>Toggle Switch {toggle ? "ON" : "OFF"}</h3> */}
+      </ToggleWrap>
+      <Wrap>
+        {data.data.map((list) => {
+          return (
+            <Article
+              key={list.id}
+              onClick={() => {
+                navigate("/detail/" + list.id);
+              }}
+            >
+              {list.bookmark ? (
                 <BookmarkFill onClick={bookMark} />
               ) : (
                 <BookmarkIcon onClick={bookMark} />
               )}
               {/* <img src={u_bookmark} alt="bookmark" style={{ width: "30px" }} onClick={bookMark}/> */}
-            </TitleWrap>
-            <p>{list.title}</p>
-            <p>{list.content}</p>
-            <ul>
-              {list.stacks.map((lang, idx) => (
-               <li key={idx}>#{lang}</li>
-                
-              ))}
-              <li>#{list.online}</li>
-            </ul>
-            <Info>
-              <Comment>
-                <img src={u_comment} alt="comment" />
-                <p>{list.commentCnt}</p>
-              </Comment>
-              <Bookmark>
-                {/* <img src={u_bookmark} alt="bookmark"/> */}
-                <BookmarkIcon />
-                <p>{list.bookmarkCnt}</p>
-              </Bookmark>
-              <p>시작예정일 : {list.startAt}</p>
-            </Info>
-          </div>
-        );
-      })}
-    </Wrap>
+              <Date>시작예정일 | {list.startAt}</Date>
+              <Content>
+                <h1>{list.title}</h1>
+                <p>{list.content}</p>
+                <Hashtag>
+                  <ul>
+                    {list.stacks.map((lang, idx) => (
+                      <li key={idx}>#{lang}</li>
+                    ))}
+                  </ul>
+                  <p style={{ color: "#ffb673" }}>#{list.online}</p>
+                </Hashtag>
+              </Content>
+
+              <Footer>
+                <User>
+                  <img src={list.profileImg} alt="profileImg" />
+                  <p>{list.nickname}</p>
+                </User>
+                <Info>
+                  <Comment>
+                    <CommentIcon />
+                    <p>{list.commentCnt}</p>
+                  </Comment>
+                  <Bookmark>
+                    <BookmarkIcon />
+                    <p>{list.bookmarkCnt}</p>
+                  </Bookmark>
+                </Info>
+              </Footer>
+            </Article>
+          );
+        })}
+      </Wrap>
+    </>
   );
 };
 const Wrap = styled.div`
+  max-width: 996px;
+  height: 100vh;
+  margin: auto;
   display: flex;
+  justify-content: space-evenly;
+  flex-wrap: wrap;
+  gap: 30px;
+  box-sizing: border-box;
 
   ul {
     display: flex;
   }
   li {
     margin-right: 5px;
-    color: #6d8dff;
+    color: #ffb673;
+  }
+  h1 {
+    font-size: 25px;
+  }
+  p {
+    font-size: 15px;
   }
 `;
-const TitleWrap = styled.div`
-  display: flex;
-  align-items: center;
+// 토글 스위치
+const ToggleWrap = styled.div`
+display:flex;
+align-items:center;
 `;
-const Img = styled.img`
-  width: 40px;
-  height: 40px;
-  border-radius: 50%;
+const ToggleBtn = styled.button`
+  width: 60px;
+  height: 30px;
+  border-radius: 30px;
+  border: none;
+  cursor: pointer;
+  background-color: ${(props) => (props.toggle ? "#ffb673" : "none")};
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: all 0.5s ease-in-out;
+`;
+const Circle = styled.div`
+  background-color: white;
+  width: 26px;
+  height: 26px;
+  border-radius: 50px;
+  position: absolute;
+  left: 5%;
+  transition: all 0.5s ease-in-out;
+  ${(props) =>
+    props.toggle &&
+    css`
+      transform: translate(30px, 0);
+      transition: all 0.5s ease-in-out;
+    `}
+`;
+// 
+const Article = styled.div`
+  flex-wrap: wrap;
+  background-color: #fff;
+  padding: 16px 20px;
+  box-shadow: 0px 0px 12px rgba(0, 0, 0, 0.08);
+  border-radius: 16px;
+  width: 30%;
+  height: 320px;
+  position: relative;
 `;
 
-const Stack = styled.li`
+const Date = styled.p`
+ color: #8B8B8B;
+ display:flex;
+ justify-content:flex-end;
+
+`;
+const Content = styled.div`
+margin :20px 0;
+h1{
+padding-bottom:20px;
+
+}
+`;
+
+const Hashtag = styled.div`
+position:absolute;
+bottom:70px;
+
+`;
+const Footer = styled.div`
   display: flex;
-  background-color: yellow;
+  width: 85%;
+  justify-content: space-between;
+  align-items: center;
+  position:absolute;
+  bottom:20px;
+
+  svg {
+    margin-right: 5px;
+  }
+`;
+const User = styled.div`
+  display: flex;
+  align-items: center;
+  img {
+    width: 30px;
+    height: 30px;
+    border-radius: 50%;
+    margin-right: 10px;
+  }
 `;
 
 const Comment = styled.div`
   display: flex;
+  margin-right: 10px;
 `;
 
 const Bookmark = styled.div`
@@ -140,5 +239,7 @@ const Bookmark = styled.div`
 const Info = styled.div`
   display: flex;
 `;
+
+
 
 export default Main;
