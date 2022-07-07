5314bcfd16208406b8e5483aab1036f561792830
sparrowscout <sparrowscout.0000@gmail.com>
2022-07-08 03:59:32 +0900

    [Update] Write.js

--- src/atom/userQuery.js
+++ src/atom/userQuery.js
@@ -1,6 +1,18 @@
+import axios from "axios";
 import { atom } from "recoil";
+
+export const token = localStorage.getItem('token')
+
+export const instance = axios.create({
+    baseURL: 'http://13.125.213.81/',
+    headers: {'Authorization': `Bearer ${token}`}
+  });
+
+  const GetUserInfo = async () => {
+    return await instance.get(`http://13.125.213.81/user/userinfo`)
+ }
 
 export const UserInfoAtom = atom({
     key:"UserInfo",
-    default:[],
+    default:GetUserInfo(),
 });
