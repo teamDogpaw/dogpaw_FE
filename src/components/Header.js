9478180ecc5cbc9ab3fe0ee0c30439ddc80f79a5
LeeHuynSuk <gusehf321@naver.com>
2022-07-12 06:09:39 +0900

    [update] 헤더 conflict 해결

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,26 +5,20 @@ import ModalOpen from "./Modal";
 import axios from "axios";
 import { Link } from "react-router-dom";
 
-
 import { ReactComponent as Logo } from "../styles/icon/logoLight.svg";
-import lightMode  from "../styles/icon/toggleLight.svg";
-import darkMode  from "../styles/icon/toggleDark.svg";
-import sun  from "../styles/icon/sun.svg";
-import moon  from "../styles/icon/moon.svg";
+import lightMode from "../styles/icon/toggleLight.svg";
+import darkMode from "../styles/icon/toggleDark.svg";
+import sun from "../styles/icon/sun.svg";
+import moon from "../styles/icon/moon.svg";
 import styled, { css } from "styled-components";
 import { useNavigate } from "react-router-dom";
 
-
 const Header = () => {
-  
   const navigate = useNavigate();
-  const [isDark,setIsDark] = useRecoilState(DarkThemeAtom);
-
-
+  const [isDark, setIsDark] = useRecoilState(DarkThemeAtom);
 
   const logout = async () => {
-    localStorage.removeItem("token"); 
-
+    localStorage.removeItem("token");
 
     localStorage.removeItem("retoken");
     localStorage.removeItem("id");
@@ -42,33 +36,34 @@ const Header = () => {
   };
   return (
     <Wrap>
-      <Logo onClick={()=>{navigate("/")}}/>
+      <Logo
+        onClick={() => {
+          navigate("/");
+        }}
+      />
       <Link to="/mypage">마이페이지</Link>
 
-    
       <ModeBtn onClick={() => setIsDark((prev) => !prev)} isDark={isDark}>
-        <ModeCircle isDark={isDark}/>
+        <ModeCircle isDark={isDark} />
       </ModeBtn>
-    
-      <ModalOpen/>
+
+      <ModalOpen />
       <button onClick={logout}>로그아웃</button>
-      
     </Wrap>
   );
 };
 
 const Wrap = styled.div`
-background: #FFFFFF;
-box-shadow: 0px 4px 12px rgba(0, 0, 0, 0.1);
-height:70px;
-margin-bottom:50px;
-display: flex;
-
-
+  background: #ffffff;
+  box-shadow: 0px 4px 12px rgba(0, 0, 0, 0.1);
+  height: 70px;
+  margin-bottom: 50px;
+  display: flex;
 `;
 
 const ModeBtn = styled.button`
-  background-image: url(${(props) => (props.isDark ? `${darkMode}` : `${lightMode}`)});
+  background-image: url(${(props) =>
+    props.isDark ? `${darkMode}` : `${lightMode}`});
   background-repeat: no-repeat;
   background-size: cover;
 
@@ -84,7 +79,6 @@ const ModeBtn = styled.button`
   align-items: center;
   transition: all 0.5s ease-in-out;
 `;
-
 
 const ModeCircle = styled.div`
   display: flex;
@@ -108,8 +102,6 @@ const ModeCircle = styled.div`
       transform: translate(-35px, 0);
       transition: all 0.4s ease-in-out;
     `}
-
 `;
-
 
 export default Header;
