d0228bf2a16318b4972362f412cfc7bba7986253
sparrowscout <sparrowscout.0000@gmail.com>
2022-07-20 04:56:15 +0900

    [Refactor] 북마크 api

--- src/api/project.js
+++ src/api/project.js
@@ -3,7 +3,7 @@ import instance from "../shared/axios";
 export const projectApis = {
     getApplicantLists : async (postId) => await instance.get(`api/allApplicants/info/${postId}`),
     getParticipantsLists : async (postId) => await instance.get(`/api/user/team/${postId}`),
-    rejectApply : async (userId, postId) => await instance.delete(`api/applicant/${userId}/rejection/${postId}`),
-    acceptApply :async (userId, postId) => await instance.post(`api/applicant/${userId}/acceptance/${postId}`),
-    explusionMate : async (userId, postId) => await instance.delete(`/api/expulsion/${userId}/teammate/${postId}`)
+    rejectApply : async (data) => await instance.delete(`api/applicant/${data.userId}/rejection/${data.postId}`),
+    acceptApply :async (data) => await instance.post(`api/applicant/${data.userId}/acceptance/${data.postId}`),
+    explusionMate : async (data) => await instance.delete(`/api/expulsion/${data.userId}/teammate/${data.postId}`)
 }
