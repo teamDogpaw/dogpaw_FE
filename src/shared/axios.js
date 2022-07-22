c623c5529c12fb16ffb43454d2c1ef43ec4882e3
sparrowscout <sparrowscout.0000@gmail.com>
2022-07-22 22:21:38 +0900

    [Error] token

--- src/shared/axios.js
+++ src/shared/axios.js
@@ -3,10 +3,11 @@ const token = localStorage.getItem("token");
 
 const baseURL = process.env.REACT_APP_BASE_URL
 
-const instance = axios.create({
-  baseURL: baseURL,
-  headers: token ? { Authorization: `Bearer ${token}` } : {},
-});
+const instance = axios.create({baseURL});
+
+if(token){
+    instance.defaults.headers.common['Authorization'] = `Bearer ${token}`
+}
 
 instance.interceptors.response.use((response) => {
   return response;
