8db6ac1948733fa8a287117b7eaa1e694e7d3704
sparrowscout <sparrowscout.0000@gmail.com>
2022-07-22 21:23:28 +0900

    [Update] headers

--- src/shared/axios.js
+++ src/shared/axios.js
@@ -3,7 +3,10 @@ const token = localStorage.getItem("token");
 
 const baseURL = process.env.REACT_APP_BASE_URL
 
-const instance = axios.create({baseURL});
+const instance = axios.create({
+  baseURL: baseURL,
+  headers: token ? { Authorization: `Bearer ${token}` } : {},
+});
 
 instance.interceptors.response.use((response) => {
   return response;
