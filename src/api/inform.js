b96b185047022d46912b36043cf0b723a4968731
jeehye03 <ks6247@naver.com>
2022-07-26 00:47:05 +0900

    [Create] api

---
+++ src/api/inform.js
@@ -0,0 +1,9 @@
+import instance from "../shared/axios";
+
+export const informApis = {
+    notification: async () => await instance.get("/notifications"),
+    notificationRead: async (notificationId) => await instance.post(`notification/read/${notificationId}`),
+    notificationCnt: async () => await instance.get("/notifications/count"),
+    notificationDeleteAll: async () => await instance.delete("notifications/delete"),
+    notificationDelete: async (notificationId) => await instance.delete(`notifications/delete/${notificationId}`)
+}
