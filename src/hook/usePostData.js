60cb060f964d9a82bfb7ed2cd33c8ae678419d26
jeehye03 <ks6247@naver.com>
2022-08-02 16:46:59 +0900

    [Update] 데이터 캐시타임 설정

--- src/hook/usePostData.js
+++ src/hook/usePostData.js
@@ -1,10 +1,16 @@
-import { useInfiniteQuery, useMutation, useQuery } from "react-query";
-import { postApis } from "../api/post";
+import { useInfiniteQuery, useMutation, useQuery } from 'react-query';
+import { postApis } from '../api/post';
 
 export function useGetPost(postId) {
-  return useQuery("detailPost", () => {
-    return postApis.getPostList(postId);
-  });
+  return useQuery(
+    'detailPost',
+    () => {
+      return postApis.getPostList(postId);
+    },
+    {
+      cacheTime: 0,
+    },
+  );
 }
 
 export function useDeletePost() {
@@ -12,24 +18,26 @@ export function useDeletePost() {
 }
 
 export function useGetBookmarkRank() {
-  return useQuery("bookmarkRank",postApis.getBookmarkRank);
+  return useQuery('bookmarkRank', postApis.getBookmarkRank);
 }
 
-export function usePostDeadline(){
-  return useMutation(postApis.postDeadlineProject,{
-    onError:() => {
-      alert("모집인원이 모두 찼을 경우 모집마감취소가 불가능합니다. \n모집인원을 늘리거나 팀원 조정이 필요합니다.")
-    }
+export function usePostDeadline() {
+  return useMutation(postApis.postDeadlineProject, {
+    onError: () => {
+      alert(
+        '모집인원이 모두 찼을 경우 모집마감취소가 불가능합니다. \n모집인원을 늘리거나 팀원 조정이 필요합니다.',
+      );
+    },
   });
 }
 
 export function useGetKeepPostList() {
   return useInfiniteQuery(
-    "postList",
+    'postList',
     ({ pageParam = 0 }) => postApis.getKeepPostList(pageParam),
     {
       getNextPageParam: (lastPage) =>
         !lastPage.isLast ? lastPage.nextPage : undefined,
-    }
+    },
   );
 }
