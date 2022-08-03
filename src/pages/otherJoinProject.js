8f42b4236f6e445ab7e0b701057131429ebc2bbf
sparrowscout <sparrowscout.0000@gmail.com>
2022-08-04 01:40:33 +0900

    [Update] OtherJoinProject

--- src/pages/otherJoinProject.js
+++ src/pages/otherJoinProject.js
@@ -3,8 +3,6 @@ import MyPagePostList from '../components/mypage/MyPagePostList';
 import { MypagePostBox } from '../styles/style';
 
 const OtherJoinProjects = ({ currentTab, data }) => {
-  console.log(data);
-
   if (data.length === 0) {
     return <EmptyPage message={'아직 참여한 프로젝트가 없습니다!'} />;
   }
